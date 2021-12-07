427239b7f20ec2b4d3e0af56a46cc14b938ba5aa
michael-steele-nox <michael.steele.nox@gmail.com>
2021-12-07 16:40:18 +0300

    added header block
    added header block

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(function(){
 	
 	$('.header-slider').slick({
-		
+		dots: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/header/arrow-up.svg" alt="arrow up"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/header/arrow-down.svg" alt="arrow down"></button>',
+		vertical: true,
+		infinite: true,
 	})
 	
 	

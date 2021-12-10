c398d00b9b4c6efd0afb8e9215ce7d2f62af3cee
michael-steele-nox <48181422+michael-steele-nox@users.noreply.github.com>
2021-12-10 23:23:34 +0300

    added next block
    added next block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-	
+$(function () {
+
 	$('.header-slider').slick({
 		dots: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/header/arrow-up.svg" alt="arrow up"></button>',
@@ -7,6 +7,31 @@ $(function(){
 		vertical: true,
 		infinite: true,
 	})
+
+	$('.product__name').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.product__content',
+		focusOnSelect: true,
+		vertical: true,
+		prevArrow: '<button type="button" class="product-prev"><img src="images/main/product/arrow-up.svg" alt="arrow up"></button>',
+		nextArrow: '<button type="button" class="product-next"><img src="images/main/product/arrow-down.svg" alt="arrow down"></button>',
+//		infinite: true,
+//		centerMode: true,
+
+	});
 	
-	
+	$('.product__content').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.product__name',
+//		fade: true,
+		vertical: true,
+		arrows: false,
+//		infinite: true,
+
+
+	});
+
+
 });
